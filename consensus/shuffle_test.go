6dbc7b7f932c6e586524ce8cf9b8018295c4d13c
colinleeky <colinleeky@gmail.com>
2022-01-22 19:24:28 +0800

    add basic frand shuffle testing

---
+++ consensus/shuffle_test.go
@@ -0,0 +1,23 @@
+package consensus
+
+import (
+	"fmt"
+	"testing"
+
+	"lukechampine.com/frand"
+)
+
+func TestBasicFrandShuffle(t *testing.T) {
+	records := []string{"a", "b", "c", "e", "f"}
+
+	fmt.Println(records)
+
+	rng := frand.NewCustom([]byte("7c85524a3202e3a2f135262c3f403982"), 1024, 12)
+
+	rng.Shuffle(len(records), func(i, j int) {
+		records[i], records[j] = records[j], records[i]
+	})
+
+	fmt.Println(records)
+
+}

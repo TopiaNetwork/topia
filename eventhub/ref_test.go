3d05b8e844920f1eead2ae750689b0631336a0d1
colinleeky <95838413+colinleeky@users.noreply.github.com>
2022-03-12 16:02:42 +0800

    add event hub

---
+++ eventhub/ref_test.go
@@ -0,0 +1,17 @@
+package eventhub
+
+import (
+	"fmt"
+	tptypes "github.com/TopiaNetwork/topia/common/types"
+	"reflect"
+	"testing"
+)
+
+func TestRef(t *testing.T) {
+	ty := reflect.TypeOf(tptypes.Block{})
+	typeName := ty.Name()
+	typeString := ty.String()
+	kindString := ty.Kind().String()
+
+	fmt.Printf("typeName=%s, typeString=%s, kindString=%s\n", typeName, typeString, kindString)
+}

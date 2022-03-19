6286f495a96504a47f08bfba234b5918d5649dae
vangjvn <vangjvn@gmail.com>
2022-03-19 11:09:10 +0800

    codetest

--- transaction/blockstoreinfo.go
+++ transaction/blockstoreinfo.go
@@ -4,6 +4,5 @@ import "github.com/TopiaNetwork/topia/common/types"
 
 type BlockResultStoreInfo struct {
 	types.BlockResult
-	TxResults map[TxKey]*TransactionResult
+	TxResults map[string]*TransactionResult
 }
-

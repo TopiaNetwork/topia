9225380e6ca3550f4a105b0af0332190fc6a9204
sz <841258081@qq.com>
2022-03-31 16:02:38 +0800

    use stretchr/testify

--- crypt/bls12381/bls12381_test.go
+++ crypt/bls12381/bls12381_test.go
@@ -2,68 +2,55 @@ package bls12381
 
 import (
 	"bytes"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGeneratePriPubKey(t *testing.T) {
 	var c CryptServiceBLS12381
 	_, _, err := c.GeneratePriPubKey()
-	if err != nil {
-		t.Error("TestGeneratePriPubKey failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestGeneratePriPubKey failed.")
 }
 
 func TestConvertToPublic(t *testing.T) {
 	var c CryptServiceBLS12381
 	sec, pub, err := c.GeneratePriPubKey()
-	if err != nil {
-		t.Error("TestConvertToPublic GeneratePriPubKey failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestConvertToPublic GeneratePriPubKey failed.")
+
 	pubConvert, err := c.ConvertToPublic(sec)
-	if err != nil {
-		t.Error("TestConvertToPublic ConvertToPublic failed: ", err)
-	}
-	if bytes.Equal(pub, pubConvert) != true {
-		t.Error("TestConvertToPublic ConvertToPublic failed.")
-	}
+	assert.Equal(t, nil, err, "TestConvertToPublic ConvertToPublic failed.")
+	assert.Equal(t, true, bytes.Equal(pub, pubConvert), "TestConvertToPublic ConvertToPublic failed.")
 }
 
 func TestSign(t *testing.T) {
 	var c CryptServiceBLS12381
 	sec, _, err := c.GeneratePriPubKey()
-	if err != nil {
-		t.Error("TestSign GeneratePriPubKey failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestSign GeneratePriPubKey failed.")
+
 	msgToSign := "This is the msg for sign."
 	_, err = c.Sign(sec, []byte(msgToSign))
-	if err != nil {
-		t.Error("TestSign Sign failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestSign Sign failed.")
 }
 
 func TestVerify(t *testing.T) {
 	var c CryptServiceBLS12381
 	sec, pub, err := c.GeneratePriPubKey()
-	if err != nil {
-		t.Error("TestVerify GeneratePriPubKey failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestVerify GeneratePriPubKey failed.")
+
 	msgToSign := "This is the msg for sign."
 	sig, err := c.Sign(sec, []byte(msgToSign))
-	if err != nil {
-		t.Error("TestVerify Sign failed: ", err)
-	}
-	if b, err := c.Verify(pub, []byte(msgToSign), sig); b != true || err != nil {
-		t.Error("TestVerify Verify failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestVerify Sign failed.")
+
+	b, err := c.Verify(pub, []byte(msgToSign), sig)
+	assert.Equal(t, true, b, "TestVerify Verify failed.")
+	assert.Equal(t, nil, err, "TestVerify Verify failed.")
 }
 
 func TestCreateAddress(t *testing.T) {
 	var c CryptServiceBLS12381
 	_, pub, err := c.GeneratePriPubKey()
-	if err != nil {
-		t.Error("TestCreateAddress GeneratePriPubKey failed: ", err)
-	}
-	if _, err := c.CreateAddress(pub); err != nil {
-		t.Error("TestCreateAddress CreateAddress failed: ", err)
-	}
+	assert.Equal(t, nil, err, "TestCreateAddress GeneratePriPubKey failed.")
+
+	_, err = c.CreateAddress(pub)
+	assert.Equal(t, nil, err, "TestCreateAddress CreateAddress failed.")
 }

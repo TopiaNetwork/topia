bb3f96e20a3fc93c1c4c6dabe395b9ec9b957cdd
sz <841258081@qq.com>
2022-03-31 16:03:27 +0800

    add benchmark

---
+++ crypt/bls12381/bls12381_benchmark_test.go
@@ -0,0 +1,41 @@
+package bls12381
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func BenchmarkSign(b *testing.B) {
+	b.StopTimer()
+	var c CryptServiceBLS12381
+	sec, _, err := c.GeneratePriPubKey()
+	assert.Equal(b, nil, err, "BenchmarkSign GeneratePriPubKey failed.")
+	msgToSign := "This is the msg for sign."
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		_, err = c.Sign(sec, []byte(msgToSign))
+		b.StopTimer()
+		assert.Equal(b, nil, err, "BenchmarkSign Sign failed.")
+
+	}
+}
+
+func BenchmarkVerify(b *testing.B) {
+	b.StopTimer()
+	var c CryptServiceBLS12381
+	sec, pub, err := c.GeneratePriPubKey()
+	assert.Equal(b, nil, err, "BenchmarkVerify GeneratePriPubKey failed.")
+
+	msgToSign := "This is the msg for sign."
+	sig, err := c.Sign(sec, []byte(msgToSign))
+	assert.Equal(b, nil, err, "BenchmarkVerify Sign failed.")
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		bo, err := c.Verify(pub, []byte(msgToSign), sig)
+		b.StopTimer()
+		assert.Equal(b, true, bo, "BenchmarkVerify Verify failed.")
+		assert.Equal(b, nil, err, "BenchmarkVerify Verify failed.")
+	}
+
+}

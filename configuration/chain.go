b8442f5e51d0f9a0e6b6a005c65b3a8e92b81198
colinleeky <colinleeky@gmail.com>
2022-05-02 14:35:06 +0800

    add deploy transaction basic disposing

--- configuration/chain.go
+++ configuration/chain.go
@@ -2,10 +2,12 @@ package configuration
 
 type ChainConfiguration struct {
 	MaxTargetItem uint64
+	MaxCodeSize   uint64 //unit: byte
 }
 
 func DefChainConfiguration() *ChainConfiguration {
 	return &ChainConfiguration{
 		MaxTargetItem: 50,
+		MaxCodeSize:   10_000_000, //10MB
 	}
 }

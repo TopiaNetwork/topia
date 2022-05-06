9114ae4cee8fe914b60316387b06b89d6baf182f
colinleeky <colinleeky@gmail.com>
2022-05-06 20:27:36 +0800

    add account permission extra disposings and account contract

--- account/permission_contract.go
+++ account/permission_contract.go
@@ -1,8 +1,8 @@
 package account
 
 import (
+	"encoding/json"
 	"reflect"
-	"sync"
 
 	tpcmm "github.com/TopiaNetwork/topia/common"
 	tpcrtypes "github.com/TopiaNetwork/topia/crypt/types"
@@ -10,26 +10,27 @@ import (
 
 type PermissionContractMethod struct {
 	GasLimit   uint64
-	sync       sync.RWMutex
-	methodsMap map[string][]string //Contract Address->Methods
+	MethodsMap map[string][]string //Contract Address->Methods
 }
 
 func NewPermissionContractMethod(gasLimit uint64) Permission {
 	return &PermissionContractMethod{
-		methodsMap: make(map[string][]string),
+		GasLimit:   gasLimit,
+		MethodsMap: make(map[string][]string),
 	}
 }
 
-func (pr *PermissionContractMethod) IsRoot() bool {
+func (pm *PermissionContractMethod) IsRoot() bool {
 	return false
 }
 
 func (pm *PermissionContractMethod) AddMethod(contractAddr tpcrtypes.Address, method string) {
-	pm.sync.Lock()
-	defer pm.sync.Unlock()
+	if method == "" {
+		return
+	}
 
 	addrStr, _ := contractAddr.HexString()
-	if methods, ok := pm.methodsMap[addrStr]; ok {
+	if methods, ok := pm.MethodsMap[addrStr]; ok {
 		if !tpcmm.IsContainString(method, methods) {
 			methods = append(methods, method)
 		}
@@ -37,21 +38,15 @@ func (pm *PermissionContractMethod) AddMethod(contractAddr tpcrtypes.Address, me
 }
 
 func (pm *PermissionContractMethod) RemoveMethod(contractAddr tpcrtypes.Address, method string) {
-	pm.sync.Lock()
-	defer pm.sync.Unlock()
-
 	addrStr, _ := contractAddr.HexString()
-	if methods, ok := pm.methodsMap[addrStr]; ok {
+	if methods, ok := pm.MethodsMap[addrStr]; ok {
 		tpcmm.RemoveIfExistString(method, methods)
 	}
 }
 
 func (pm *PermissionContractMethod) HasMethodPerm(contractAddr tpcrtypes.Address, method string) bool {
-	pm.sync.RLock()
-	defer pm.sync.RUnlock()
-
 	addrStr, _ := contractAddr.HexString()
-	if methods, ok := pm.methodsMap[addrStr]; ok {
+	if methods, ok := pm.MethodsMap[addrStr]; ok {
 		if tpcmm.IsContainString(method, methods) {
 			return true
 		}
@@ -60,11 +55,37 @@ func (pm *PermissionContractMethod) HasMethodPerm(contractAddr tpcrtypes.Address
 	return false
 }
 
-func (pr *PermissionContractMethod) SamePerm(other Permission) bool {
+func (pm *PermissionContractMethod) SamePerm(other Permission) bool {
 	perm, ok := other.(*PermissionContractMethod)
 	if !ok {
 		return false
 	}
 
-	return reflect.DeepEqual(pr.methodsMap, perm.methodsMap)
+	return reflect.DeepEqual(pm.MethodsMap, perm.MethodsMap)
+}
+
+func (pm *PermissionContractMethod) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		GasLimit   uint64
+		MethodsMap map[string][]string
+	}{
+		GasLimit:   pm.GasLimit,
+		MethodsMap: pm.MethodsMap,
+	})
+}
+
+func (pm *PermissionContractMethod) UnmarshalJSON(data []byte) error {
+	var pmObj struct {
+		GasLimit   uint64
+		MethodsMap map[string][]string
+	}
+	err := json.Unmarshal(data, &pmObj)
+	if err != nil {
+		return err
+	}
+
+	pm.GasLimit = pmObj.GasLimit
+	pm.MethodsMap = pmObj.MethodsMap
+
+	return nil
 }
--- vm/native/contract/account.go
+++ vm/native/contract/account.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/TopiaNetwork/topia/account"
+	tpacc "github.com/TopiaNetwork/topia/account"
 	tpcrtypes "github.com/TopiaNetwork/topia/crypt/types"
 	tpvmcmm "github.com/TopiaNetwork/topia/vm/common"
 )
@@ -12,24 +12,106 @@ import (
 type ContractAccount struct {
 }
 
-func (a *ContractAccount) BindName(ctx context.Context, addr tpcrtypes.Address, accountName account.AccountName) error {
+func (a *ContractAccount) BindName(ctx context.Context, parentAddr tpcrtypes.Address, addr tpcrtypes.Address, accountName tpacc.AccountName) error {
 	cCtx := tpvmcmm.NewContractContext(ctx)
-
-	fromAddr, err := cCtx.GetFromAccount()
-	if err != nil {
-		return err
-	}
-
-	if !accountName.IsChild(fromAddr.Name) {
-		return fmt.Errorf("Bind account name is not child  of from account: bind addr %d name %s, from addr %d name %s")
-	}
 
 	vmServant, err := cCtx.GetServant()
 	if err != nil {
 		return fmt.Errorf("Can't get vm servant: err %v", err)
 	}
+	parentAcc, err := vmServant.GetAccount(parentAddr)
+	if err != nil {
+		return fmt.Errorf("Can't get parent account: err %v", err)
+	}
+
+	if !accountName.IsChild(parentAcc.Name) {
+		return fmt.Errorf("Bind account name is not child of account: bind addr %d name %s, parent addr %d name %s", addr, accountName, parentAddr, parentAcc.Name)
+	}
 
 	return vmServant.UpdateName(addr, accountName)
+}
 
-	return nil
+func (a *ContractAccount) grantAccessOperation(ctx context.Context, parentAddr tpcrtypes.Address, addr tpcrtypes.Address, operation func(acc *tpacc.Account) error) error {
+	cCtx := tpvmcmm.NewContractContext(ctx)
+
+	vmServant, err := cCtx.GetServant()
+	if err != nil {
+		return fmt.Errorf("Can't get vm servant: err %v", err)
+	}
+	parentAcc, err := vmServant.GetAccount(parentAddr)
+	if err != nil {
+		return fmt.Errorf("Can't get parent account: err %v, addr %s", err, parentAddr)
+	}
+
+	acc, err := vmServant.GetAccount(parentAddr)
+	if err != nil {
+		return fmt.Errorf("Can't get account: err %v, addr %s", err, addr)
+	}
+
+	if !acc.Name.IsChild(parentAcc.Name) {
+		return fmt.Errorf("The account name is not child of account: addr %d name %s, parent addr %d name %s", addr, acc.Name, parentAddr, parentAcc.Name)
+	}
+
+	err = operation(acc)
+	if err != nil {
+		return err
+	}
+
+	return vmServant.UpdateAccount(acc)
+}
+
+func (a *ContractAccount) GrantAccessMethods(ctx context.Context, parentAddr tpcrtypes.Address, addr tpcrtypes.Address, contractAddr tpcrtypes.Address, methods []string, gasLimit uint64) error {
+	return a.grantAccessOperation(ctx, parentAddr, addr, func(acc *tpacc.Account) error {
+		accToken := acc.Token
+		if accToken == nil {
+			perm := tpacc.NewPermissionContractMethod(gasLimit)
+			accToken = tpacc.NewAccountToken(perm)
+
+			acc.Token = accToken
+		}
+
+		for _, method := range methods {
+			acc.Token.Permission.AddMethod(contractAddr, method)
+		}
+
+		return nil
+	})
+}
+
+func (a *ContractAccount) GrantAccessRoot(ctx context.Context, parentAddr tpcrtypes.Address, addr tpcrtypes.Address) error {
+	return a.grantAccessOperation(ctx, parentAddr, addr, func(acc *tpacc.Account) error {
+		accToken := acc.Token
+		if accToken == nil {
+			perm := tpacc.NewPermissionRoot()
+			accToken = tpacc.NewAccountToken(perm)
+
+			acc.Token = accToken
+		}
+
+		return nil
+	})
+}
+
+func (a *ContractAccount) RevokeGrantAccess(ctx context.Context, parentAddr tpcrtypes.Address, addr tpcrtypes.Address) error {
+	return a.grantAccessOperation(ctx, parentAddr, addr, func(acc *tpacc.Account) error {
+		accToken := acc.Token
+		if accToken != nil {
+			acc.Token = nil
+		}
+
+		return nil
+	})
+}
+
+func (a *ContractAccount) RevokeGrantAccessMethods(ctx context.Context, parentAddr tpcrtypes.Address, addr tpcrtypes.Address, contractAddr tpcrtypes.Address, methods []string) error {
+	return a.grantAccessOperation(ctx, parentAddr, addr, func(acc *tpacc.Account) error {
+		accToken := acc.Token
+		if accToken != nil {
+			for _, method := range methods {
+				acc.Token.Permission.RemoveMethod(contractAddr, method)
+			}
+		}
+
+		return nil
+	})
 }

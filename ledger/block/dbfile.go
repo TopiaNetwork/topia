3a752a1283816a604fd9c5ba541e731f890c8781
yiranlandtour <yiranlandtour@163.com>
2022-06-13 09:33:16 +0800

    change code

--- ledger/block/dbfile.go
+++ ledger/block/dbfile.go
@@ -2,8 +2,16 @@ package block
 
 
 import (
+	"fmt"
+	"github.com/TopiaNetwork/topia/chain/types"
 	"launchpad.net/gommap"
+	tplgtypes "github.com/TopiaNetwork/topia/ledger/types"
+	"log"
 	"os"
+	"path"
+	"syscall"
+	"encoding/json"
+
 	//"syscall"
 )
 
@@ -11,22 +19,24 @@ const LOCK_FILE = "LOCK"
 const DATA_FILE = "DATA"
 const MERGE_FILE = "DATA.MER"
 
-type DBFile struct {
+type TopiaFile struct {
 	File   *os.File
 	Offset int64
 }
 
-func newDBDataFile(basePath string) (*DBFile, error) {
-	datafile := basePath + string(os.PathSeparator) + DATA_FILE
-	return newFileImpl(datafile)
-}
+//func newTopiaFile(basePath string) (*TopiaFile, error) {
+//	datafile := basePath + string(os.PathSeparator) + DATA_FILE
+//	return newFileImpl(datafile)
+//}
 
 //func newDBMergeFile(basePath string) (*DBFile, error) {
 //	datafile := basePath + string(os.PathSeparator) + MERGE_FILE
 //	return newFileImpl(datafile)
 //}
 
-func newFileImpl(filepath string) (*DBFile, error) {
+func newDataFile(block *types.Block) (*TopiaFile, error) {
+	filepath := path.Join(block.GetHash() ,".topia")
+
 	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		return nil, err
@@ -37,34 +47,72 @@ func newFileImpl(filepath string) (*DBFile, error) {
 		return nil, err
 	}
 
-	return &DBFile{
+	return &TopiaFile{
 		File:   file,
 		Offset: stat.Size(),
 	}, nil
 }
 
-func (df *DBFile) Write(e *DbItem) error {
-	encode, err := e.Encode()
-	if err != nil {
-		return err
-	}
+func newIndexFile(block *types.Block) (*TopiaFile, error) {
+	filepath := path.Join(block.GetHash() ,".index")
 
-	_, err = gommap.Map(f.Fd(), gommap.PROT_WRITE, gommap.MAP_PRIVATE)
-	if err != nil {
-		return err
-	}
-
-	df.Offset += int64(e.Size())
-	return nil
-}
-
-func (df *DBFile) ReadItem(offset int64) (*DbItem, error) {
-	buf := make([]byte, DbItemHdrSize)
-	if _, err := gommap.Map(f.Fd(), gommap.PROT_READ, gommap.MAP_PRIVATE)
-
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		return nil, err
 	}
 
+	stat, err := os.Stat(filepath)
+	if err != nil {
+		return nil, err
+	}
 
+	return &TopiaFile{
+		File:   file,
+		Offset: stat.Size(),
+	}, nil
+}
+
+func (df *TopiaFile) Writedata(block *types.Block) error {
+
+	mmap, _ := gommap.MapAt(0,file.Fd(), 0,100,syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	defer mmap.UnsafeUnmap()
+
+
+	newFsConfigBytes, _ := json.Marshal(block)
+	//_, err = file.Seek(int64(len(newFsConfigBytes)), 1)
+	//if err != nil {
+	//	log.Fatal("Failed to seek")
+	//}
+	for i:=0;i<len(newFsConfigBytes);i++ {
+		mmap[i] = newFsConfigBytes[i]
+		mmap.Sync(syscall.MS_SYNC)
+
+		err = file.Sync()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println("", i)
+	}
+}
+
+//func (df *TopiaFile) ReadItem(offset int64) (*DbItem, error) {
+//	buf := make([]byte, DbItemHdrSize)
+//	if _, err := gommap.Map(f.Fd(), gommap.PROT_READ, gommap.MAP_PRIVATE)
+//
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//
+//}
+func outSize(filepath string)(bool,error){
+	stat, err := os.Stat(filepath)
+	if err != nil {
+		return false, err
+	}
+	if stat.Size() > 1000000 {
+		return false,nil
+	}
+	return true,nil
 }
--- ledger/block/mmap.go
+++ ledger/block/mmap.go
@@ -7,15 +7,15 @@ import (
 )
 
 func system_entrance(){
-	sysType := runtime.GOOS
-
-	if sysType == 'linux' {
-		// LINUX系统
-		fmt.Println("Linux system")
-	}
-
-	if sysType == 'windows' {
-		// windows系统
-		fmt.Println("win system")
-	}
+	//sysType := runtime.GOOS
+	//
+	//if sysType == 'linux' {
+	//	// LINUX系统
+	//	fmt.Println("Linux system")
+	//}
+	//
+	//if sysType == 'windows' {
+	//	// windows系统
+	//	fmt.Println("win system")
+	//}
 }

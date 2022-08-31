8ba7d3fd0151a7487d6a6b9fb684938dd84ae5e9
sz <841258081@qq.com>
2022-08-31 15:24:37 +0800

    add lru cache

---
+++ wallet/cache/cache_lru.go
@@ -0,0 +1,107 @@
+package cache
+
+import (
+	"errors"
+	"github.com/TopiaNetwork/topia/wallet/key_store"
+	lru "github.com/hashicorp/golang-lru"
+	"sync"
+)
+
+type AddrItemInCache struct {
+	EncKeyItem []byte
+
+	lock bool
+}
+
+const addrCacheSize = 512 // hold 512 item max
+var addrCache, _ = lru.New(addrCacheSize)
+
+var (
+	enableCacheMutex sync.RWMutex
+	enable_Cache     = true // default enabled
+)
+
+var (
+	addrNotExistErr = errors.New("addr doesn't exist in cache")
+)
+
+func GetAddrLock(addr string) (bool, error) {
+	value, ok := addrCache.Get(addr)
+	if !ok {
+		return false, addrNotExistErr
+	}
+	item, ok := value.(AddrItemInCache)
+	if !ok {
+		return false, errors.New("invalid addr item type")
+	}
+	return item.lock, nil
+}
+
+func SetAddrLock(addr string, set bool) error {
+	value, ok := addrCache.Get(addr)
+	if !ok {
+		return addrNotExistErr
+	}
+	item, ok := value.(AddrItemInCache)
+	if !ok {
+		return errors.New("invalid addr item type")
+	}
+	item.lock = set
+	addrCache.Add(addr, item)
+
+	return nil
+}
+
+func SetAddrToCache(addr string, encData []byte) {
+	item := AddrItemInCache{
+		EncKeyItem: encData,
+		lock:       false,
+	}
+	addrCache.Add(addr, item)
+}
+
+func GetAddrFromCache(addr string) (AddrItemInCache, error) {
+	value, ok := addrCache.Get(addr)
+	if !ok {
+		return AddrItemInCache{}, addrNotExistErr
+	}
+	item, ok := value.(AddrItemInCache)
+	if !ok {
+		return AddrItemInCache{}, errors.New("invalid addr item type")
+	}
+	return item, nil
+}
+
+func SetEnableToCache(set bool) {
+	enableCacheMutex.Lock()
+	enable_Cache = set
+	enableCacheMutex.Unlock()
+}
+
+func GetEnableFromCache() (ret bool) {
+	enableCacheMutex.RLock()
+	ret = enable_Cache
+	enableCacheMutex.RUnlock()
+
+	return ret
+}
+
+func GetKeysFromCache() (addrs []string) {
+	keys := addrCache.Keys()
+	addrs = make([]string, len(keys))
+	for i := range keys {
+		addrs[i] = keys[i].(string)
+	}
+
+	return addrs
+}
+
+func RemoveFromCache(key string) {
+	if key == key_store.EnableKey {
+		enableCacheMutex.Lock()
+		enable_Cache = false
+		enableCacheMutex.Unlock()
+	} else {
+		addrCache.Remove(key)
+	}
+}

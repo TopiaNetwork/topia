ac6a2d7e33eb1c7676708c431c02fdb55b3321fc
sz <841258081@qq.com>
2023-02-16 16:02:12 +0800

    add: subscription message filter module

--- api/rpc/filter/filter.go
+++ api/rpc/filter/filter.go
@@ -1,15 +1,14 @@
-package rpc
+package filter
 
 import (
+	"errors"
+	"fmt"
 	"github.com/TopiaNetwork/topia/eventhub"
 	"reflect"
+	"strconv"
 	"strings"
+	"time"
 )
-
-// filterString应该做个长度限制
-
-// 1. 收到订阅请求，词法分析 (判断是否含有次字段，判断操作符是否是那几个之一，判断) 检查filterString是否合法
-// 2.
 
 type filter struct {
 	eventName  string
@@ -17,16 +16,23 @@ type filter struct {
 	directPass bool          //
 }
 
-func newFilter(eventName string, filterString string) (*filter, error) {
+func NewFilter(eventName string, filterString string) (*filter, error) {
 	p := new(parser)
 	f, err := p.parseFilterString(eventName, filterString)
 	if err != nil {
 		return nil, err
 	}
 
+	err = f.completeAndValidateConditions()
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+
 }
 
-func (f *filter) pass(msg eventhub.EventMsg) bool {
+func (f *filter) Pass(msg eventhub.EventMsg) bool {
 	if f.directPass {
 		return true
 	}
@@ -46,7 +52,7 @@ func (f *filter) pass(msg eventhub.EventMsg) bool {
 		var clausePass = true
 
 		for _, cond := range clause {
-			if !cond.pass(evDataT,evDataV) {
+			if !cond.pass(evDataT, evDataV) {
 				clausePass = false
 				break
 			}
@@ -60,50 +66,175 @@ func (f *filter) pass(msg eventhub.EventMsg) bool {
 	return false
 }
 
-// filter condition
-type condition struct {
-	field    string // fieldName of eventMsg.Data
-	operator string //
-	data     interface{}
-	dataType reflect.Kind
-}
+//
+func (f *filter) completeAndValidateConditions() error {
+	for i, clause := range f.clauses {
+		for j, cond := range clause {
+			kind, ok := isValidFieldName(f.eventName, cond.field)
+			if !ok {
+				return errors.New("not a valid field name or eventMsg not been registered")
+			}
 
-func (c *condition) pass(evDataT reflect.Type, evDataV reflect.Value) bool {
-	// msg.Data 可能是个指针，也可能是个结构体。condition 所对应的field可能在结构体（指针）的内层
+			legal := cond.operator.isLegal()
+			if !legal {
+				return errors.New("illegal operator")
+			}
 
-	// 1. 反射msg, 根据c.field名称找到对应字段
-	// 2. 判断c.operator,
+			var dataString string
+			if cond.operator != Exists {
+				dataString, ok = cond.data.(string)
+				if !ok {
+					return errors.New("unexpected data type")
+				}
+			} else { // operator is Exists
+				continue // no need to complete data and dataType
+			}
 
-	// 根据字段名获取字段
-	for i := 0; i < evDataT.NumField(); i++ {
-		if strings.ToLower(evDataT.Field(i).Name) == c.field {
-			if evDataV.Field(i).Kind() == c.dataType {
-				switch c.dataType {
-				case reflect.Int:
-					evDataV.Field(i).Int()
-
+			switch kind {
+			case reflect.Int:
+				intNum, err := strconv.Atoi(dataString)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = intNum
+			case reflect.Int8:
+				parsedInt, err := strconv.ParseInt(dataString, 10, 8)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = int8(parsedInt)
+			case reflect.Int16:
+				parsedInt, err := strconv.ParseInt(dataString, 10, 16)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = int16(parsedInt)
+			case reflect.Int32:
+				parsedInt, err := strconv.ParseInt(dataString, 10, 32)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = int32(parsedInt)
+			case reflect.Int64:
+				parsedInt, err := strconv.ParseInt(dataString, 10, 64)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = int64(parsedInt)
+			case reflect.Uint:
+				parsedUint, err := strconv.ParseUint(dataString, 10, 64)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = uint(parsedUint)
+			case reflect.Uint8:
+				parsedUint, err := strconv.ParseUint(dataString, 10, 8)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = uint8(parsedUint)
+			case reflect.Uint16:
+				parsedUint, err := strconv.ParseUint(dataString, 10, 16)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = uint16(parsedUint)
+			case reflect.Uint32:
+				parsedUint, err := strconv.ParseUint(dataString, 10, 32)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = uint32(parsedUint)
+			case reflect.Uint64:
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
 				}
 
+				if strings.Contains(cond.field, "timestamp") {
+
+					if parsedTime, err := time.Parse("2006-01-02T15:04:05.000", dataString); err == nil {
+						f.clauses[i][j].data = uint64(parsedTime.UnixNano())
+					} else if parsedTime, err = time.Parse("2006-01-02", dataString); err == nil {
+						if cond.operator == Eq || cond.operator == Ne || cond.operator == Lt || cond.operator == Lte {
+							f.clauses[i][j].data = uint64(parsedTime.UnixNano())
+						} else if cond.operator == Gt || cond.operator == Gte {
+							parsedTime = parsedTime.Add(24 * time.Hour).Add(-1 * time.Millisecond)
+							f.clauses[i][j].data = uint64(parsedTime.UnixNano())
+						} else {
+							return errors.New("timestamp don't support operator: " + cond.operator.string())
+						}
+					} else {
+						return errors.New("illegal time format: " + dataString)
+					}
+
+				} else {
+					parsedUint, err := strconv.ParseUint(dataString, 10, 64)
+					if err != nil {
+						return err
+					}
+					f.clauses[i][j].data = uint64(parsedUint)
+				}
+
+			case reflect.Float32:
+				parsedFloat, err := strconv.ParseFloat(dataString, 32)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = float32(parsedFloat)
+			case reflect.Float64:
+				parsedFloat, err := strconv.ParseFloat(dataString, 64)
+				if err != nil {
+					return err
+				}
+				if cond.operator == Contains || cond.operator == Exists {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = parsedFloat
+			case reflect.String:
+				if cond.operator != Eq && cond.operator != Ne && cond.operator != Contains {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+			case reflect.Slice:
+				if cond.operator != Eq && cond.operator != Ne && cond.operator != Contains {
+					return fmt.Errorf("dataType: %s cannot support operation: %s", kind.String(), cond.operator.string())
+				}
+				f.clauses[i][j].data = []byte(dataString)
+			default:
+				return errors.New("unexpected type: " + kind.String())
 			}
-			
-		} else if
+
+			f.clauses[i][j].dataType = kind
+		}
 	}
-
-}
-
-
-
-type parser struct {
-	i            int
-	filterString string
-}
-
-func (p *parser) parseFilterString(eventName string, filterString string) (*filter, error) {
-	if len(strings.TrimSpace(filterString)) == 0 {
-		return &filter{eventName: eventName, directPass: true}, nil
-	}
-	// TODO 检查 filterString 长度
-
-	p.filterString = filterString
-
+	return nil
 }
